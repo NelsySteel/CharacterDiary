f8f22b6e6cd1fe94c4e0d22a0afbd1d5a072484d
NelsySteel <100653681+MamaevaKristy@users.noreply.github.com>
2022-07-10 15:58:46 +0300

    Includes fix

--- Source/CharacterDiary/Private/DnD/DnDSkill.cpp
+++ Source/CharacterDiary/Private/DnD/DnDSkill.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "DnD/Public/DnDSkill.h"
+#include "DnD/DnDSkill.h"
 
 #include "DnD/DNDSkillsSystemComponent.h"
 
